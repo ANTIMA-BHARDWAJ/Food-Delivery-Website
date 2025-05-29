875985b7f02bdbcb457ad1c698e806afccfbb8b3
ANTIMA-BHARDWAJ <162999970+ANTIMA-BHARDWAJ@users.noreply.github.com>
2025-05-29 02:11:06 -0400

    Update Home.jsx
    litle change

--- Home.jsx
+++ Home.jsx
@@ -7,14 +7,14 @@ import { food_items } from './food'
 
 
 function Home() {
-  let[cate,setCate]=useState(food_items)
+  let[cate, setCate]= useState(food_items);
 
-function filter(category){
-  if (category ==="All"){
-    setCate(food_items)
+function filter(Category) {
+  if (Category === "All") {
+    setCate(food_items);
   } else {
-    let newList=food_items.filter((item)=>( item.food-category === category));
-    setCate(newList)
+   let newList = food_items.filter((item) => item.food_type === Category);
+    setCate(newList);
   }
 }
 
@@ -22,17 +22,18 @@ function filter(category){
     <div className='w-full min-h-screen bg-slate-200'>
       <Navbar/>
       <div className='flex  flex-wrap-reverse justify-center items-center gap-6 w-[100%]'>
-        {categories.map((item) =>{
-          return (<div  key={item.name}
-            className='w-[140px] h-[150px] bg-white flex flex-col items-start gap-5 p-5 justify-start text-[20px] font-semibold text-gray-500 rounded-lg shadow-xl hover:bg-green-200 cursor-pointer transition-all duration-200' onClick={() => filter(item.name)}>
+        {categories.map((item) =>(
+          <div key={item.name}className='w-[140px] h-[150px] bg-white flex flex-col items-start gap-5 p-5 justify-start text-[20px] font-semibold text-gray-500 rounded-lg shadow-xl hover:bg-green-200 cursor-pointer transition-all duration-200' onClick={() => filter(item.name)}>
             {item.Image}
             {item.name} 
-            </div>)
-        })}
+            </div>
+        ))}
       </div>
   <div className='w-full flex flex-wrap gap-5 justify-center items-center px-5 pt-8'>
     {cate.map((item)=>(
-      <Card key={item.id} name ={item.food_name} image={item.food_image} price={item.price} id={item.id} type={item.food_type}/>
+      <Card
+      key={item.id}
+      name ={item.food_name} image={item.food_image} price={item.price} id={item.id} type={item.food_type}/>
     ))}
   </div>
     </div>
