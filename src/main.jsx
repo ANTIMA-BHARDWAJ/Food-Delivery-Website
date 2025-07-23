aa19ec1ef20da091ea95e61ad4c1a5b950adb787
ANTIMA-BHARDWAJ <162999970+ANTIMA-BHARDWAJ@users.noreply.github.com>
2025-07-23 01:26:43 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { dataContext } from './context/userContext';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
+  <dataContext.Provider value={{}}>
     <App />
-  </StrictMode>,
-)
+  </dataContext.Provider>
+  </Provider>
+);
